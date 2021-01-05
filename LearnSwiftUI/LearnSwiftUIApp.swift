5a4f7684f0ab327fa937410b5e96e76ea09a44c4
danielwpeters0623 <64049081+danielwpeters0623@users.noreply.github.com>
2021-01-05 15:54:03 -0500

    First Commit

---
+++ LearnSwiftUI/LearnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUIApp.swift
+//  LearnSwiftUI
+//
+//  Created by Daniel Peters on 1/4/21.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
